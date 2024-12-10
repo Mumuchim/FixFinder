2c818d5a8fc79ce0b397d712045d0a9765d29261
sadlyy14 <paulinekayecarag3@gmail.com>
2024-12-10 11:46:15 +0800

    Update stud_history.php

--- stud_history.php
+++ stud_history.php
@@ -62,7 +62,7 @@ $con->close();
     <title>Student History</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <<link rel="stylesheet" href="css/history.css"> 
 </head>
 <body>
     <div class="container">
